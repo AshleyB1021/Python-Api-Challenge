bef2f4813d219c1f6e2a6d8bfc9ad8d141caf901
AshleyB1021 <142064579+AshleyB1021@users.noreply.github.com>
2023-10-01 08:35:52 -0400

    updated gitignore

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,7 @@
+# Adding config.py file.
+api_keys.py
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "500b46890cfb40ffb8e1a0c9b697b210"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "500b46890cfb40ffb8e1a0c9b697b210"
